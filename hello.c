70812fc2d50a13a557fdd424a3f3b230f0b0cdeb
Preethi <preethi@Preethis-MacBook-Air.local>
2018-09-21 13:57:42 +0530

    2 repeated for loops

--- hello.c
+++ hello.c
@@ -11,10 +11,16 @@ int main()
     printf("You prefer 0 or 1? : ");
     scanf("%d",&a);
     if(a==0)
+    {
+        for(i = 0; i < 5; i++)
+            printf("%d",a);
         printf("Zero!!\n");
+    }
     if(a==1)
+    {
+        for(i = 0; i < 5; i++)
+            printf("%d",a);
         printf("One!!\n");
-    
-    printf("Bye there!);
+    }
     
 }
