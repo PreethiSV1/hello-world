4adb9c1209ab640fd1f24ccd99bcca8933b2b2c7
Preethi <preethi@Preethis-MacBook-Air.local>
2018-09-21 13:30:38 +0530

    Zero or One

--- hello.c
+++ hello.c
@@ -6,4 +6,13 @@ int main()
 	scanf("%s",s);
 	printf("Hey %s!\n",s);
 	printf("Hope you will enjoy this!");
+    
+    int a;
+    printf("You prefer 0 or 1? : ");
+    scanf("%d",&a);
+    if(a==0)
+        printf("Zero!!\n");
+    if(a==1)
+        printf("One!!\n");
+    
 }
